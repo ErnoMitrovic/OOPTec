441c7537dbf025c800b2ab792b8a394a64a93368
Ernesto Miranda <ernomitrovic@zohomail.com>
2022-06-01 21:21:14 -0500

    Shipping virtual methods declared

--- Inheritance/cShipping.cpp
+++ Inheritance/cShipping.cpp
@@ -13,4 +13,16 @@ Shipping::~Shipping(){
 
 std::string Shipping::toString(){
     std::string info = "";
+    info += "- SHIPPER INFO -\n";
+    info += "NAME: " + name[0] +  "\nADDRESS:" + address[0] + "\nCITY: " + city[0] +
+    "\nSTATE: " + state[0] + "\nZIP: " + zip[0];
+    info += "- RECEIVER INFO -\n";
+    info += "NAME: " + name[1] +  "\nADDRESS:" + address[1] + "\nCITY: " + city[1] +
+    "\nSTATE: " + state[1] + "\nZIP: " + zip[1];
+    info += "\nSTANDARD COST: " + std::to_string(stdCost) + "\n";
+    return info;
+}
+
+float Shipping::calculateCost(){
+    return stdCost;
 }
